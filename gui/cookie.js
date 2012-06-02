e706d8152fbfb7db98f2103404b3b8be2fd8a3fb
stehlo <stehlo@hotmail.com>
2012-06-03 00:15:46 +0100

    fixed a few mistakes (explicit declarations); replaced deprecated functions; replaced inconsistent quotes;

--- gui/cookie.js
+++ gui/cookie.js
@@ -1,39 +1,30 @@
-function getCookie(c_name)
-{
-if (document.cookie.length>0)
-  {
-  c_start=document.cookie.indexOf(c_name + "=");
-  if (c_start!=-1)
-    { 
-    c_start=c_start + c_name.length+1 ;
-    c_end=document.cookie.indexOf(";",c_start);
-    if (c_end==-1) c_end=document.cookie.length
-    return unescape(document.cookie.substring(c_start,c_end));
-    } 
-  }
-return ""
+function getCookie(c_name) {
+	if (document.cookie.length > 0) {
+		var c_start = document.cookie.indexOf(c_name + '=');
+		if (c_start != -1) {
+			c_start = c_start + c_name.length + 1;
+			var c_end = document.cookie.indexOf(';', c_start);
+			if (c_end == -1) c_end = document.cookie.length;
+			return decodeURIComponent(document.cookie.substring(c_start, c_end));
+		}
+	}
+	return '';
 }
 
-function setCookie(c_name,value,expiredays)
-{
-var exdate=new Date();
-exdate.setDate(exdate.getDate()+expiredays);
-document.cookie=c_name+ "=" +escape(value)+((expiredays==null) ? "" : "; expires="+exdate.toUTCString());
+function setCookie(c_name, value, expiredays) {
+	var exdate = new Date();
+	exdate.setDate(exdate.getDate() + expiredays);
+	document.cookie = c_name + '=' + encodeURIComponent(value) + ((expiredays == null) ? '' : '; expires=' + exdate.toUTCString());
 }
 
-function checkCookie()
-{
-username=getCookie('username');
-if (username!=null && username!="")
-  {
-  alert('Welcome again '+username+'!');
-  }
-else 
-  {
-  username=prompt('Please enter your name:',"");
-  if (username!=null && username!="")
-    {
-    setCookie('username',username,365);
-    }
-  }
+function checkCookie() {
+	var username = getCookie('username');
+	if (username != null && username != '') {
+		alert('Welcome again ' + username + '!');
+	} else {
+		username = prompt('Please enter your name:', '');
+		if (username != null && username != '') {
+			setCookie('username', username, 365);
+		}
+	}
 }
