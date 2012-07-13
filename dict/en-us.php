4b4d5083a978eb1e518e42a2723b27c9f57ea16a
Bong Cosca <bong.cosca@yahoo.com>
2012-07-13 09:36:46 +0800

    Modified ICU unit tests

---
+++ dict/en-us.php
@@ -0,0 +1,7 @@
+<?php
+
+return array(
+	'love'=>'I love',
+	'today'=>'Today is {0,date}',
+	'tqbf'=>'The quick brown fox jumps over the lazy dog.'
+);
--- dict/es.php
+++ dict/es.php
@@ -3,8 +3,5 @@
 return array(
 	'love'=>'Me encanta',
 	'today'=>'Hoy es {0,date}',
-	'tqbf'=>'El pingüino Wenceslao hizo kilómetros bajo exhaustiva lluvia y frío, añoraba a su querido cachorro.',
-	'pi'=>'{0,number}',
-	'money'=>'{0,number,currency}'
-
+	'tqbf'=>'El pingüino Wenceslao hizo kilómetros bajo exhaustiva lluvia y frío, añoraba a su querido cachorro.'
 );
