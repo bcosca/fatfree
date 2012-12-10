3ab011e1317e600dac24a100f843d935beafbaa3
Bong Cosca <bong.cosca@yahoo.com>
2012-12-10 09:47:14 +0800

    Adjust whitespaces

--- ns/c.php
+++ ns/c.php
@@ -3,10 +3,12 @@
 namespace NS;
 
 class C {
+
 	// Emulate HTTP method so we can test map()
 	function __call($name,$args) {
 		$f3=\Base::instance();
 		$f3->set('route',strtoupper($name));
 		$f3->set('body',$f3->get('BODY'));
 	}
+
 }
