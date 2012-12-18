76407c6304e861fd58d4e0bea7ddfdb7b6d34c7b
Bong Cosca <bong.cosca@yahoo.com>
2012-12-18 16:26:30 +0800

    Update inline and API documentation

--- lib/f3.php
+++ lib/f3.php
@@ -1,6 +1,20 @@
 <?php
 
-//! Legacy mode
+/**
+	Copyright (c) 2010-2012 F3::Factory/Bong Cosca
+	All rights reserved
+
+	This file is part of the Fat-Free Framework (http://fatfree.sf.net)
+
+	THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY
+	KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+	IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR
+	PURPOSE.
+
+	Please see the license.txt file for more information.
+**/
+
+//! Legacy mode enabler
 class F3 {
 
 	static
--- lib/matrix.php
+++ lib/matrix.php
@@ -1,5 +1,19 @@
 <?php
 
+/**
+	Copyright (c) 2010-2012 F3::Factory/Bong Cosca
+	All rights reserved
+
+	This file is part of the Fat-Free Framework (http://fatfree.sf.net)
+
+	THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY
+	KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+	IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR
+	PURPOSE.
+
+	Please see the license.txt file for more information.
+**/
+
 //! Generic array utilities
 class Matrix extends Prefab {
 
