277ae80cef56a3aa9357585d500f5d736846083d
Bong Cosca <bong.cosca@yahoo.com>
2013-10-24 20:29:42 +0800

    Verify folder existence; Create if necessary

--- app/redir.php
+++ app/redir.php
@@ -5,7 +5,10 @@ namespace App;
 class Redir {
 
 	function get($f3) {
-		$f3->write($f3->get('TEMP').'redir',microtime(TRUE));
+		$tmp=$f3->get('TEMP');
+		if (!is_dir($tmp))
+			mkdir($tmp);
+		$f3->write($tmp.'redir',microtime(TRUE));
 		$f3->reroute('/router');
 	}
 
