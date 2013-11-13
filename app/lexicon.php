cb4a31444755b3c1ffb1cbfb95ccbe46e63d7d44
Bong Cosca <bong.cosca@yahoo.com>
2013-11-13 23:38:59 +0800

    Adjust test fixtures to language() behavior

--- app/lexicon.php
+++ app/lexicon.php
@@ -20,23 +20,23 @@ class Lexicon extends Controller {
 			'LANGUAGE: '.$language.' auto-detected'
 		);
 		$template=\Template::instance();
-		$f3->set('LANGUAGE','fr_FR');
+		$f3->set('LANGUAGE','fr-FR');
 		$test->expect(
 			substr_count($f3->decode($template->render('templates/lexicon.htm')),
 			'Les naïfs ægithales hâtifs pondant à Noël où il gèle sont sûrs d\'être déçus et de voir leurs drôles d\'œufs abîmés.'),
-			'fr_FR'
+			'fr-FR'
 		);
-		$f3->set('LANGUAGE','en_US');
+		$f3->set('LANGUAGE','en-US');
 		$test->expect(
 			substr_count($f3->decode($template->render('templates/lexicon.htm')),
 			'The quick brown fox jumps over the lazy dog.'),
-			'en_US'
+			'en-US'
 		);
-		$f3->set('LANGUAGE','es_CL');
+		$f3->set('LANGUAGE','es-CL');
 		$test->expect(
 			substr_count($f3->decode($template->render('templates/lexicon.htm')),
 			'El pingüino Wenceslao hizo kilómetros bajo exhaustiva lluvia y frío, añoraba a su querido cachorro.'),
-			'es_CL'
+			'es-CL'
 		);
 		$f3->set('LANGUAGE','en');
 		$test->expect(
