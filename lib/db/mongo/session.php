9b9220fcc3faa278a78ec427d9f2cf40ffde0c63
Bong Cosca <bong.cosca@yahoo.com>
2014-01-12 11:45:35 +0800

    Bug fix: $this context

--- lib/db/mongo/session.php
+++ lib/db/mongo/session.php
@@ -156,29 +156,35 @@ class Session extends Mapper {
 		);
 		register_shutdown_function('session_commit');
 		@session_start();
+		$self=$this;
 		$fw=\Base::instance();
-		$headers=$fw->get('HEADERS');
-		if (($csrf=$this->csrf()) &&
-			((!isset($_COOKIE['_']) || $_COOKIE['_']!=$csrf) ||
-			($ip=$this->ip()) && $ip!=$fw->get('IP') ||
-			($agent=$this->agent()) && !isset($headers['User-Agent']) ||
-				$agent!=$headers['User-Agent'])) {
-			$jar=$fw->get('JAR');
-			$jar['expire']=strtotime('-1 year');
-			call_user_func_array('setcookie',
-				array_merge(array('_',''),$jar));
-			unset($_COOKIE['_']);
-			session_destroy();
-			\Base::instance()->error(403);
-		}
-		$csrf=$fw->hash($fw->get('ROOT').$fw->get('BASE')).'.'.
-			$fw->hash(mt_rand());
-		if ($this->load(array('session_id'=>session_id()))) {
-			$this->set('csrf',$csrf);
-			$this->save();
-			call_user_func_array('setcookie',
-				array('_',$csrf)+$fw->get('JAR'));
-		}
+		$out=$fw->mutex('csrf',function() use($fw,$self) {
+			$headers=$fw->get('HEADERS');
+			if (($csrf=$self->csrf()) &&
+				((!isset($_COOKIE['_']) || $_COOKIE['_']!=$csrf) ||
+				($ip=$self->ip()) && $ip!=$fw->get('IP') ||
+				($agent=$self->agent()) && !isset($headers['User-Agent']) ||
+					$agent!=$headers['User-Agent'])) {
+				$jar=$fw->get('JAR');
+				$jar['expire']=strtotime('-1 year');
+				call_user_func_array('setcookie',
+					array_merge(array('_',''),$jar));
+				unset($_COOKIE['_']);
+				session_destroy();
+				return FALSE;
+			}
+			$csrf=$fw->hash($fw->get('ROOT').$fw->get('BASE')).'.'.
+				$fw->hash(mt_rand());
+			if ($self->load(array('session_id'=>session_id()))) {
+				$self->set('csrf',$csrf);
+				$self->save();
+				call_user_func_array('setcookie',
+					array('_',$csrf)+$fw->get('JAR'));
+			}
+			return TRUE;
+		});
+		if (!$out)
+			$fw->error(403);
 	}
 
 }
