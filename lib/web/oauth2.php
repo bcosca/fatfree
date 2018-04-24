b170060c2bf3d2739fa6b74dca17dcd017212a9d
John <bucs-fan813@users.noreply.github.com>
2018-04-23 20:10:30 -0400

    fixes oauth2 returns null #1104

--- lib/web/oauth2.php
+++ lib/web/oauth2.php
@@ -64,10 +64,13 @@ class OAuth2 extends \Magic {
 		$response=$web->request($uri,$options);
 		if ($response['error'])
 			user_error($response['error'],E_USER_ERROR);
-		return $response['body'] &&
-			preg_grep('/HTTP\/1\.\d 200/',$response['headers'])?
-			json_decode($response['body'],TRUE):
-			FALSE;
+		if (isset($response['body']) && preg_grep('/HTTP\/1\.\d 200/',$response['headers']))
+		{
+		    if ($token = json_decode($response['body'],TRUE))
+		        return $token;
+		    return $response['body'];
+		}
+			return FALSE;
 	}
 
 	/**
