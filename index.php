c04ed01a571d4e19b86f9772fa067e3407c9f86b
ikkez <ikkez0n3@gmail.com>
2022-02-16 16:31:57 +0100

    update composer settings

--- index.php
+++ index.php
@@ -1,7 +1,16 @@
 <?php
 
-// Kickstart the framework
-$f3=require('lib/base.php');
+if (file_exists('vendor/autoload.php')) {
+	// load via composer
+	require_once('vendor/autoload.php');
+	$f3 = \Base::instance();
+} elseif (!file_exists('lib/base.php')) {
+	die('fatfree-core not found. Run `git submodule init` and `git submodule update` or install via composer with `composer install`.');
+} else {
+	// load via submodule
+	/** @var Base $f3 */
+	$f3=require('lib/base.php');
+}
 
 $f3->set('DEBUG',1);
 if ((float)PCRE_VERSION<8.0)
